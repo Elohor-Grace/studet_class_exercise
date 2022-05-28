e85aaa84275501efe80bf7cb8ada361bd2e280e5
Elohor-Grace <85343998+Elohor-Grace@users.noreply.github.com>
2022-05-28 11:31:33 -0700

    Add files via upload
    student class exercise

---
+++ main.py
@@ -0,0 +1,37 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score,):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+        #pass
+
+def change_name(self, new_name):
+    self.name = new_name
+    new_name = "peter"
+    print(f"the student new name is {new_name}")
+
+def change_age(self, new_age):
+    self.age = new_age
+    new_age = "34"
+    print(f"the student new age is {new_age}")
+
+def add_tracks(self, new_tracks):
+    self.tracks = new_tracks
+    new_tracks = ("ui/ux")
+    print(f"the student new track is {new_tracks}")
+
+def get_score(self, new_score):
+    self.score = new_score
+    new_score = 2.5
+    print(f"the student new score is {new_score}")
+    
+    
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_tracks("UI/UX")
+Bob.get_score(2.5)
